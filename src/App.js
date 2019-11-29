54e72c154c82000ea43a67b0285710ca62645193
belindabee <belindabee42@gmail.com>
2019-11-29 22:47:57 +0700

    make it link

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react'
-import 
+import Navbar from './Design/Navbar'
+
+//scss
+
 
 export default class App extends Component {
   render() {
     return(
       <div>
-        <h1>HAI</h1>
+        <Navbar />
       </div>
     )
   }
--- src/Design/Navbar.js
+++ src/Design/Navbar.js
@@ -1,29 +1,25 @@
 import React, {Component} from 'react'
+import '../asset/scss/Navbar.scss'
+
+//pic
+import konshinbukai from '../asset/images/konshinbukai.png'
 
 export default class Navbar extends Component {
     render() {
         return(
             <div>
                 <div>
-                    <div className='logo'><img src='koshinbukai-icon1.png' alt='logo' /></div>
+                    <div><img src={konshinbukai} alt='logo'  className='logo' />Budo Academy</div>
                 </div>
-                <div>
-                    Home
-                </div>
-                <div>
-                    Tentang Budo Academy
-                </div>
-                <div>
-                    Program
-                </div>
-                <div>
-                    Anak Berkebutuhan Khusus
-                </div>
-                <div>
-                    Instruktur
-                </div>
-                
+                    <div className='navbar'>
+                        <div className='grid1'>Home</div>
+                        <div className='grid2'>Tentang Budo Academy</div>
+                        <div className='grid3'>Program</div>
+                        <div className='grid4'>Anak Berkebutuhan Khusus</div>
+                        <div>Instruktur</div>
+                    </div>
             </div>
+
         )
     }
 }
